9fc7585d3ce13a4bf0d897d7d993aa32fd1544e6
Andreas Bernacca <hello@rinti.se>
2026-01-04 18:50:39 +0100

    Initial Commit

---
+++ PhotoTriageUITests/PhotoTriageUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoTriageUITestsLaunchTests.swift
+//  PhotoTriageUITests
+//
+//  Created by Andreas Bernacca on 2026-01-04.
+//
+
+import XCTest
+
+final class PhotoTriageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
